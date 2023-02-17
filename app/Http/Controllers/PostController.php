718b814879861e5f5a98dd875032d4e91a2d9000
Arif P <marifp@staff.undip.ac.id>
2023-02-17 15:50:04 +0700

    17 02 2023 UI Search

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,17 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
+
 
 class PostController extends Controller
 {
     public function index()
     {
+        $title = '';
+        if (request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            $title = 'in ' . $category->name;
+        }
+        if (request('author')) {
+            $author = User::firstWhere('username', request('author'));
+            $title = 'by ' . $author->name;
+        }
+
+        // dd(request('search'));
+
         return view('posts', [
 
-            "title"     => "All Posts",
+            "title"     => "All Blog . $title",
             "active"     => 'blog',
-            // "posts"     => Post::all()
-            "posts"     => Post::latest()->get()
+            "posts"     => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString()
         ]);
     }
 
@@ -23,7 +37,7 @@ class PostController extends Controller
     {
         return view('post', [
 
-            "title" => "Single Post",
+            "title" => "Single Blog",
             "active"     => 'blog',
             "post"  => $post
         ]);
